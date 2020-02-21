2a7f2298a6ac40c05947fb2908cc8c2c77987eed
yaxx <shehuzaks@yahoo.com>
2020-02-21 05:51:00 -0800

    change to remote url

--- client/src/app/util/url.ts
+++ client/src/app/util/url.ts
@@ -1,2 +1,2 @@
-//export const host = 'http://192.168.1.101:5000';
-   export const host = 'http://localhost:5000';
+export const host = 'http://3.21.76.116:5000';
+   //export const host = 'http://localhost:5000';
